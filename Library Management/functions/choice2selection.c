1d303b0278770c149cd92901a322308cf3a7e7cb
Aryan Rastogi <134607843+Arystar01@users.noreply.github.com>
2023-10-28 19:52:41 +0530

    Created a basic switch cases with , file for each switch case selection

---
+++ Library Management/functions/choice2selection.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+int main(){
+    int choice2a;
+    do {
+        printf("1. Add a new book. \n");
+        printf("2. Search a book. \n");
+        printf("3. Delete a book. \n");
+        printf("4. Display all books. \n");
+        printf("5. Exit \n");
+        printf("Enter your choice: ");
+        scanf("%d",&choice2a);
+        switch(choice2a){
+             case 1:
+
+        break;
+        case 2:
+
+        break;
+        case 3:
+
+        break;
+        case 4 :
+
+        break;
+        case 5:
+
+        break;
+        default:
+        printf("Enter Valid choice. \n");
+        break;
+
+
+        }
+    }while(choice2a!=5);
+    
+}
